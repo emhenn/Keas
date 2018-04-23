943d7b274f39d989a557fb57ccd1a5132090e1e2
Unknown <jscubbage@ucdavis.edu>
2018-04-23 13:15:39 -0700

    Start notificaiton service

---
+++ Keas.Mvc/Services/NotificaitonService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keas.Mvc.Services
+{
+    public class NotificaitonService
+    {
+    }
+}
