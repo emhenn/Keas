23805b8ef16c1f7e8d63ef8db4a7c1e2f0aa5b2b
Unknown <jscubbage@ucdavis.edu>
2018-04-23 14:52:36 -0700

    First taks of eventService

---
+++ Keas.Mvc/Services/EventService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Keas.Core.Domain;
+
+namespace Keas.Mvc.Services
+{
+    public interface IEventService
+    {
+        Task TrackCreateKey(Key key, User user);
+
+    }
+    public class EventService : IEventService
+    {
+        private readonly HistoryService _historyService;
+        private readonly NotificaitonService _notificaitonService;
+
+        public EventService(HistoryService historyService, NotificaitonService notificaitonService)
+        {
+            _historyService = historyService;
+            _notificaitonService = notificaitonService;
+        }
+
+        public async Task TrackCreateKey(Key key, User user)
+        {
+            var history = await _historyService.KeyCreated(key, user);
+            await _notificaitonService.KeyCreated(key, history);
+        }
+    }
+}
