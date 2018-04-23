558d7814d0b9633cac514e78b7960be4de36e7da
Unknown <jscubbage@ucdavis.edu>
2018-04-23 11:48:53 -0700

    Added for time conversions

---
+++ Keas.Core/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keas.Core.Helpers
+{
+    public static class DateTimeExtensions
+    {
+        public static readonly TimeZoneInfo Pacific = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+
+        public static DateTime? ToPacificTime(this DateTime? dateTime)
+        {
+            if (dateTime == null)
+            {
+                return null;
+            }
+            return ToPacificTime(dateTime.Value);
+        }
+        public static DateTime ToPacificTime(this DateTime dateTime)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(dateTime, Pacific);
+        }
+
+        public static DateTime FromPacificTime(this DateTime dateTime)
+        {
+            return TimeZoneInfo.ConvertTimeToUtc(dateTime, Pacific);
+        }
+
+    }
+}
